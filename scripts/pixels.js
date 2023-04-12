bd26b92aeaae2ac1693ee07dd804996ff1b234ab
Mohamed Eltuhamy <tuhamy@gmail.com>
2023-04-12 15:48:04 +0000

    Changed format

--- scripts/pixels.js
+++ scripts/pixels.js
@@ -1,81 +1,68 @@
-// Pixels
-// ------------------
-//
-// Concerned about your privacy? We do not store IP addresses, and we do not create unique cookies.
-// See: https://duck.co/help/privacy/atb
-//
-// Adapted from https://github.com/duckduckgo/Casper/blob/master/assets/js/pixels.js
-//
-// Pixel format:
-// -------------
-// Link click: helppages.link.[source].[article].[link]
-// Loading: helppages.load.[source].[article]
+/*
+Pixels
+-------
+
+Concerned about your privacy? We do not store IP addresses, and we do not create unique cookies.
+See: https://duck.co/help/privacy/atb
+
+Pixel format:
+help.[kebab-pathname].load
+help.[kebab-pathname].click?link-url=
+
+Examples:
+help.company-advertising-and-affiliates.load
+help.company-advertising-and-affiliates.click?link-url=https-reddit-com-r-duckduckgo
+*/
 
 (function ready(fn) {
   document.readyState !== 'loading' ? fn() : document.addEventListener('DOMContentLoaded', fn);
 })(function () {
-  // -------------------------
-  // I. Initialization & Setup
-  // -------------------------
+  var externalLinkAllowList = {
+    'https://about.ads.microsoft.com/en-us/h/a/microsoft-advertising': true,
+  };
 
-  // Most of the time we don't want a pixel firing
-  // several times throughout the document so we use another
-  // key-value object to keep track if a pixel has been fired or not.
-  var seenList = {};
-
-  // Page Info:
-  // ----------
-  // This section is where we get the information that we need about the page.
-  // - Where the visitor came from.
-  // - The path of the page.
-
-  // Get source param in the URL.
-  var source = /s=([^&\s]*)/.exec(location.search);
-  source = source && source[1] && decodeURIComponent(source[1]) || 'direct';
-
-  // Get path to uniquely identify pixel.
+  var hasFired = {}; // Fire pixels only once
   var pathname = location.pathname.replace(/\/(.+)\//, '$1');
-  pathname = pathname === '/' ? 'home' : pathname;
+  var basePixelUrl = 'https://improving.duckduckgo.com/t/help_' + sanitize(pathname === '/' ? 'home' : pathname);
 
-  // --------------------
-  // II. Helper Functions
-  // --------------------
+  /**
+   * Fires the pixel
+   * @param {event, extraData} ops 
+   */
+  function firePixel(event, extraData) {
+    extraData = extraData || {};
 
-  // Helper: firePixel
-  // -------------------
-  // This function abstracts away the logging details.
-  function firePixel() {
-    // This is the path to our pixels for logging certain events on
-    // the page.
-    var pixelUrl = 'https://improving.duckduckgo.com/t/helppages_';
-    var args = [].slice.call(arguments); //data + '_' + source + '_' + pathname;
-    var options = args.pop();
-    var concat = args.join('_');
-    var pixelPath = pixelUrl + concat;
-    if (options && options.once && seenList[concat]) {
+    var pixelUrl = basePixelUrl + '_' + event;
+    var extraDataString = '';
+
+    Object.keys(extraData).forEach(function (extraDataKey) {
+      extraDataString = extraDataString
+        + (extraDataString === '' ? '?' : '&')
+        + encodeURIComponent(sanitize(extraDataKey))
+        + '='
+        + encodeURIComponent(sanitize(extraData[extraDataKey]));
+    });
+
+    pixelUrl = pixelUrl + extraDataString;
+
+    if (hasFired[pixelUrl]) {
       return;
-    } else if (options && options.once) {
-      seenList[concat] = true;
     }
+    hasFired[pixelUrl] = true;
 
-    // We use the Beacon API if it's available for more accurate logging.
-    // Reference: https://developer.mozilla.org/en-US/docs/Web/API/Beacon_API
     if ('sendBeacon' in navigator) {
-      navigator.sendBeacon(pixelPath);
+      // https://developer.mozilla.org/en-US/docs/Web/API/Beacon_API
+      navigator.sendBeacon(pixelUrl);
     } else {
-      // Otherwise we go with creating an <img> element
-      // and setting the src attribute with the GIF.
+      // sendBeacon fallback: img tag
       var pixel = document.createElement('img');
-      pixel.setAttribute('src', pixelPath);
-
+      pixel.setAttribute('src', pixelUrl);
     }
   }
 
-  // Helper: sanitizeUrl
-  // Sanitzes a given URL by removing unsafe characters.
-  // E.g. 'https://reddit.com/r/duckduckgo' -> 'https-reddit-com-r-duckduckgo'
-  function sanitizeUrl(url) {
-    return url
+
+  function sanitize(str) {
+    return str
       // strip leading/trailing slash
       .replace(/^\/|\/$/, '')
       // strip unsafe chars for grafana
@@ -85,23 +72,24 @@
   }
 
   // --------------------
-  // III. Event Listeners
+  // Event Listeners
   // --------------------
 
-  // Page loads
-  // ----------
-  // Fire straight away as we're already in an onload event!
-  firePixel('load', sanitizeUrl(source), sanitizeUrl(pathname), {once: true});
+  // Page loads: Fire here as we're already in an onload event
+  firePixel('load');
 
-  // Link Clicks
-  // -----------
-  // Fire pixels when links within the article are clicked.
-  // Example: helppages.link.quora.https-spreadprivacy-com.google-filter-bubble-study
+  // Link clicks: Fire when links within the article are clicked
   Array.prototype.slice.call(document.querySelectorAll('article a'), 0).forEach(function (link) {
     link.addEventListener('click', function () {
-      firePixel('link', sanitizeUrl(source), sanitizeUrl(pathname), sanitizeUrl(link.href), {
-        once: true
-      });
+      var href = link.href;
+      var hostname = link.hostname
+      if (hostname === location.hostname || hostname === 'help.duckduckgo.com' || externalLinkAllowList[href]) {
+        // internal link clicks and allow-listed external links can send the href
+        firePixel('click', { 'link-url': href });
+      } else {
+        // otherwise we don't send the href but fire a click pixel for measuring engagement
+        firePixel('click');
+      }
     });
   });
 });
