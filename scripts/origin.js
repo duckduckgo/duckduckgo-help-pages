dafceab5d30f18055e72185e27a2d0dcfc104314
Rajat Jain <thisisrajat@users.noreply.github.com>
2024-08-28 09:10:23 +0000

    Review fixes

--- scripts/origin.js
+++ scripts/origin.js
@@ -2,6 +2,12 @@
 layout: null
 ---
 (function() {
+    if (typeof URL !== 'function') {
+        return;
+    }
+
+    var paths = ['/browser', '/mac', '/windows', '/pro', '/app', '/'];
+
     function addOrUpdateQueryParam(url, param, value) {
         if (!url || typeof url !== 'string') return url;
         
@@ -11,12 +17,18 @@ layout: null
     }
     
     
-    function shouldModifyUrl(pathname) {
+    function shouldModifyUrl(pathname, hostname) {
         if (!pathname || typeof pathname !== 'string') return false;
-        var paths = ['/browser', '/mac', '/windows', '/pro', '/app', '/'];
+        if (!hostname || typeof hostname !== 'string') return false;
+
+        if (hostname !== "duckduckgo.com") {
+            return false;
+        }
+
         for (var i = 0; i < paths.length; i++) {
             if (pathname === paths[i] || pathname.indexOf(paths[i] + '/') === 0) return true;
         }
+
         return false;
     }
     
@@ -26,14 +38,15 @@ layout: null
             var link = links[i];
             try {
                 var url = new URL(link.href, window.location.origin);
-                if (shouldModifyUrl(url.pathname)) {
+                if (shouldModifyUrl(url.pathname, url.hostname)) {
                     link.href = addOrUpdateQueryParam(link.href, 'origin', 'funnel_help');
                 } else if (url.hostname === 'play.google.com') {
                     link.href = addOrUpdateQueryParam(link.href, 'referrer', 'origin=funnel_help');
+                } else if (url.hostname === "apps.apple.com") {
+                    link.href = addOrUpdateQueryParam(link.href, 'ct', 'funnel_help')
                 }
             } catch (e) {
-                // Invalid URL, skip this link
-                console.warn('Skipping invalid URL:', link.href);
+                // Do nothing
             }
         }
     }
